ea71832946b7db59185e1e4ce044d635ac46e11e
Ryoichi Satoyama <ryoichi.satoyama@gmail.com>
2023-01-20 01:06:41 +0900

    integrated aws 2

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import styles from "../styles/Home.module.css";
 import CarPanel from "components/car-panel";
 import { getPlaiceholder } from "plaiceholder";
 import { aws_api_baseurl, aws_cloudfront_baseurl } from "libs/constants";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home(props) {
   const tomicars = props.tomicars;
@@ -20,11 +22,15 @@ export default function Home(props) {
 }
 
 export const getServerSideProps = async () => {
-  const response = await fetch(aws_api_baseurl + "tomicars");
-  const data = await response.json();
-  const objectData = JSON.parse(JSON.stringify(data));
+  // fetchはNode.js 18 だとだめ？　とりあえずVercelではだめ。Localはおけ
+  // const response = await fetch(aws_api_baseurl + "tomicars");
+
+  const response = await axios.get(aws_api_baseurl + "tomicars");
+  const objectData = await response.data;
+  // const objectData = JSON.parse(JSON.stringify(data));
   const tomicars = await Promise.all(
-    objectData.map(async (tomica) => {
+    Object.keys(objectData).map(async (index) => {
+      const tomica = objectData[index];
       const { base64 } = await getPlaiceholder(
         aws_cloudfront_baseurl + tomica.image_path
       );
@@ -36,7 +42,20 @@ export const getServerSideProps = async () => {
         blurDataURL: base64,
       };
     })
+    // objectData.map(async (tomica) => {
+    //   const { base64 } = await getPlaiceholder(
+    //     aws_cloudfront_baseurl + tomica.image_path
+    //   );
+    //   return {
+    //     no: tomica.no,
+    //     name: tomica.name_jp,
+    //     image_path: aws_cloudfront_baseurl + tomica.image_path,
+    //     sound_path: aws_cloudfront_baseurl + tomica.sound_path,
+    //     blurDataURL: base64,
+    //   };
+    // })
   );
+
   tomicars.sort(function (a, b) {
     if (a.no > b.no) {
       return 1;
